830aa25271e15567283d8143a5cf94042f45db0c
Gufderald <den17112000@gmail.com>
2022-06-07 00:58:14 +0300

    upd

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask import Flask
 
 app = Flask(__name__)
 cache = redis.Redis(host='redis', port=6379)
-someshit = open('/data/nothing').read
+someshit = open('/code/someshit/nothing').read
 
 def get_hit_count():
     retries = 5
